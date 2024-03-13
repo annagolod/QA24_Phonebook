de9474cb06fdb570935a6749e08e610e3a3ff8f8
annagolod <anna.golod@gmail.com>
2024-03-13 20:57:18 +0200

    login success

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,12 +3,22 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class ApplicationManager {
     WebDriver wd;
+    HelperUser helperUser;
+
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
 
     public void init(){
         wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wd.navigate().to("https://telranedu.web.app/home");
+        helperUser = new HelperUser(wd);
     }
 
     public void stop(){
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,36 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+    @BeforeMethod
+    public void preCondition(){
+        //if SignOut presents ----> logout
+        if (app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+
+    }
+    @Test
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("tretam0810@gmail.com", "Phone54321#");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+
+    }
 
     @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("tretam0810@gmail.com", "Phone54321#");
+        app.getHelperUser().submitLogin();
 
-    public void test(){
+        Assert.assertTrue(app.getHelperUser().isLogged());
 
     }
 }
